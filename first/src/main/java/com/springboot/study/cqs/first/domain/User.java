605ffb1ee5dd9aef594e9daea0bfeedb96d785e6
chengqingsong@chaoxing.com <chengqingsong@chaoxing.com>
2019-02-15 15:47:53 +0800

    my filter

--- first/src/main/java/com/springboot/study/cqs/first/domain/User.java
+++ first/src/main/java/com/springboot/study/cqs/first/domain/User.java
@@ -1,7 +1,15 @@
 package com.springboot.study.cqs.first.domain;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotNull
     private String userName;
+
+    @NotEmpty
+    @Size(max=8,min =6,message = "password size between 6 and 8")
     private String passWord;
 
     public User() {
@@ -26,5 +34,13 @@ public class User {
 
     public void setPassWord(String passWord) {
         this.passWord = passWord;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", passWord='" + passWord + '\'' +
+                '}';
     }
 }
--- first/src/main/java/com/springboot/study/cqs/first/web/HelloWorldController.java
+++ first/src/main/java/com/springboot/study/cqs/first/web/HelloWorldController.java
@@ -2,12 +2,17 @@ package com.springboot.study.cqs.first.web;
 
 import com.springboot.study.cqs.first.domain.User;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.List;
+
 /**
  * @author cqs
- *
  */
 @RestController
 public class HelloWorldController {
@@ -19,13 +24,39 @@ public class HelloWorldController {
 
     /**
      * spring boot 对对象 json的支持
+     *
      * @return
      */
     @RequestMapping("getUser")
-    public User getUser(){
+    public User getUser() {
         User user = new User();
         user.setUserName("cqs");
         user.setPassWord("123456");
         return user;
+    }
+
+    /**
+     * 参数验证
+     */
+    @RequestMapping("saveUser")
+    public void saveUser(@Valid User user, BindingResult result) {
+        System.out.println("user:" + user);
+        if (result.hasErrors()) {
+            /**
+             * 显示校验关键字的异常信息，但获取不了对象属性字段
+             */
+            List<ObjectError> errors = result.getAllErrors();
+            for (ObjectError error : errors) {
+                System.out.println("校验Code：" + error.getCode() + " 异常：" + error.getDefaultMessage());
+            }
+
+            /**
+             * 显示对象属性字段的异常信息
+             */
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            for (FieldError error : fieldErrors) {
+                System.out.println("字段：" + error.getField() + " 异常：" + error.getDefaultMessage());
+            }
+        }
     }
 }
